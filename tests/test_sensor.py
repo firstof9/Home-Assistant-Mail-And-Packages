03e34360940ede88af1958309119ef6219751688
firstof9 <firstof9@gmail.com>
2020-09-18 19:29:36 -0700

    WIP Tests

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,60 +1,12 @@
 """ Test Mail and Packages Sensor """
-import datetime
-import email
-from custom_components.mail_and_packages.const import DOMAIN
-
 from pytest_homeassistant_custom_component.common import MockConfigEntry
+from tests.common import setup_mnp
 
 
-async def setup_mnp(hass):
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        title="imap.test.email",
-        data={
-            "amazon_fwds": '""',
-            "folder": '"INBOX"',
-            "generate_mp4": false,
-            "gif_duration": 5,
-            "host": "imap.test.email",
-            "image_path": "/config/www/mail_and_packages/",
-            "image_security": true,
-            "password": "suchfakemuchpassword",
-            "port": 993,
-            "resources": [
-                "amazon_packages",
-                "fedex_delivered",
-                "fedex_delivering",
-                "fedex_packages",
-                "mail_updated",
-                "ups_delivered",
-                "ups_delivering",
-                "ups_packages",
-                "usps_delivered",
-                "usps_delivering",
-                "usps_mail",
-                "usps_packages",
-                "zpackages_delivered",
-                "zpackages_transit",
-                "dhl_delivered",
-                "dhl_delivering",
-                "dhl_packages",
-                "amazon_delivered",
-            ],
-            "scan_interval": 20,
-            "username": "user@fake.email",
-        },
-    )
+async def test_sensor(hass, generic_data):
+    await setup_mnp(hass, fixture=generic_data)
 
-    entry.add_to_hass(hass)
-    await hass.config_entries.async_setup(entry.entry_id)
-    await hass.async_block_till_done()
+    state = hass.states.get("sensor.mail_updated")
 
-
-async def test_sensor(hass):
-    await setup_mnp(hass)
-
-    test_message = email.message.Message()
-    test_message["From"] = "sender@test.com"
-    test_message["Subject"] = "Test"
-    test_message["Date"] = datetime.datetime.now()
-    test_message.set_payload("Test Message")
+    assert state
+    assert state.state == "Sep-18-2020 06:29 PM"
